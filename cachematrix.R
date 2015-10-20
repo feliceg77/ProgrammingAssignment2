b14c2f96b5d6563b7bc3d2f6dff42302c5c12380
Felice <feliceg77@gmail.com>
2015-10-20 20:29:08 +0200

    ultimaversione

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the inverse of a Matrix
+## This functions cache the inverse of a matrix rather than compute it repeatedly.
 
-## Write a short comment describing this function
+
+## Clean cache if the matrix is not equal to the last has already been calculated.
+## Cache the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-	m <- NULL
+	
+	matequal <- function(z, t) is.matrix(t) && is.matrix(z) && dim(t) == dim(z) && all(t == z)
+
+	if ( is.null(matrice) || !matequal(x,matrice)){
+		m <- NULL
+	}
 	set <- function(y) {
 		x <<- y
 		m <<- NULL
 	}
 	get <- function() x
-	setInverseMatrix <- function(mean) m <<- mean
+	setInverseMatrix <- function(matriceInversa) {
+		m <<- matriceInversa
+		matrice <<- x 
+	}
 	getInverseMatrix <- function() m
 	list(set = set, get = get,
              setInverseMatrix = setInverseMatrix,
@@ -18,11 +28,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x' using cache
 
-cacheSolve <- function(x, ...) {
-	## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { 
+
 	m <- x$getInverseMatrix()
+
 	if(!is.null(m)) {
 		message("getting cached data")
 		return(m)
